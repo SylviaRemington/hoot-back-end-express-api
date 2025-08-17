24b336845e1283438f7f36537ed0e5a35687c81f
SylviaRemington <sylviaremington@gmail.com>
2025-08-17 09:07:31 -0700

    correcting server.js code - this is the corrected code starting Aug 17th

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-// https://github.com/SylviaRemington/hoot-back-end-express-api.git
-
+// npm
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -8,23 +7,37 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 
+// Import routers
+const authRouter = require('./controllers/auth');
+const testJwtRouter = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
-// Routes go here
+// Routes
+app.use('/auth', authRouter);
+app.use('/test-jwt', testJwtRouter);
+app.use('/users', usersRouter);
 
+// Start the server and listen on port 3000
 app.listen(3000, () => {
   console.log('The express app is ready!');
 });
 
+
 // Note to self regarding package.json - In scripts fyi: 
 // start is for running the server in production mode. 
 // dev is for running the server in development mode with nodemon.
+
+
 
